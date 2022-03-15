99407160a5f7d38963940931bbc556b24d849eac
Firdavs <firdavssherzodovic@gmail.com>
2022-03-15 14:00:15 +0500

    lal

---
+++ out/objetmain.js
@@ -0,0 +1,16 @@
+"use strict";
+//types
+// type Blog ={
+//     name: string,
+//     date: number,
+//     rasmi: string,
+//     matn: string,
+//     avtori: AVTOR 
+// }
+let objes = {
+    xona: 'fizika',
+    snifnomi: '6a',
+    ism: ['ali', 'bali', 'vali'],
+    raxbar: 'Jalol'
+};
+console.log(objes);
---
+++ src/objetmain.ts
@@ -0,0 +1,52 @@
+//types
+// type Blog ={
+//     name: string,
+//     date: number,
+//     rasmi: string,
+//     matn: string,
+//     avtori: AVTOR 
+// }
+
+// type AVTOR ={
+//     ismi: string,
+//     kasbi: string,
+//     rasmi: string,
+//     reyting: number 
+
+
+// }
+
+
+// let obj: Blog ={
+// name:"suv",
+// date: 45,
+// rasmi:"cola",
+// matn: "fanta",
+// avtori:{
+//     ismi:"sprayt",
+//     kasbi:"pasi",
+//     rasmi:"olma",
+//     reyting: 35
+
+// }
+// }
+// console.log(obj);
+
+
+
+type maksin ={
+    xona: string,
+    snifnomi: string,
+    ism: string [],
+    raxbar: string
+
+}
+let objes ={
+    xona:'fizika',
+    snifnomi:'6a',
+    ism:['ali','bali','vali'],
+    raxbar:'Jalol'
+
+}
+console.log(objes);
+
